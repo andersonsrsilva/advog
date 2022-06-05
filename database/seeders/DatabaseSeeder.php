24ee8e1e34e4673e1f482e8fdfaceef6ee3bebb7
andersonsrsilva <andersonsrsilva@gmail.com>
2022-06-05 15:13:43 -0300

    Add code uf

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        //  $this->call(UsersSeeder::class);
-        //  $this->call(RolesSeeder::class);
-        //  $this->call(UsersRolesSeeder::class);
+         $this->call(UsersSeeder::class);
+         $this->call(RolesSeeder::class);
+         $this->call(UsersRolesSeeder::class);
          $this->call(UfTableSeeder::class);
          $this->call(CityAcTableSeeder::class);
          $this->call(CityAlTableSeeder::class);
@@ -22,5 +22,25 @@ class DatabaseSeeder extends Seeder
          $this->call(CityBaTableSeeder::class);
          $this->call(CityCeTableSeeder::class);
          $this->call(CityDfTableSeeder::class);
+         $this->call(CityEsTableSeeder::class);
+         $this->call(CityGoTableSeeder::class);
+         $this->call(CityMaTableSeeder::class);
+         $this->call(CityMtTableSeeder::class);
+         $this->call(CityMsTableSeeder::class);
+         $this->call(CityMgTableSeeder::class);
+         $this->call(CityPaTableSeeder::class);
+         $this->call(CityPbTableSeeder::class);
+         $this->call(CityPrTableSeeder::class);
+         $this->call(CityPeTableSeeder::class);
+         $this->call(CityPiTableSeeder::class);
+         $this->call(CityRjTableSeeder::class);
+         $this->call(CityRnTableSeeder::class);
+         $this->call(CityRsTableSeeder::class);
+         $this->call(CityRoTableSeeder::class);
+         $this->call(CityRrTableSeeder::class);
+         $this->call(CityScTableSeeder::class);
+         $this->call(CitySpTableSeeder::class);
+         $this->call(CitySeTableSeeder::class);
+         $this->call(CityToTableSeeder::class);
     }
 }
